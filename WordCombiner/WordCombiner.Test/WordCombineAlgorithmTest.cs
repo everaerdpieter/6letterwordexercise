31ccb6f15f20c7fd90dbc6072c408823a3e89c29
Pieter Everaerd <pieter.everaerd@attentia.be>
2020-05-05 15:32:09 +0200

    Init project structure

---
+++ WordCombiner/WordCombiner.Test/WordCombineAlgorithmTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCombiner.Test
+{
+    [TestClass]
+    public class WordCombineAlgorithmTest
+    {
+      [TestMethod]
+      public void Todo() {
+        throw new NotImplementedException();
+      }
+    }
+}
